caf87f487442c3f53a07ea03b07495c7f323b1cf
Arsal Asif <arsalasif@users.noreply.github.com>
2022-09-03 19:29:56 -0400

    Update path to `test_graphs`

--- src/project/graph/theory/Main.java
+++ src/project/graph/theory/Main.java
@@ -13,8 +13,8 @@ public class Main {
 		GraphColoring graph;
 		try
 		{
-			String fileName = "Wheel6SquareSpoke.txt";
-			String path = Paths.get("").toAbsolutePath().toString()+"/example_graphs/" + fileName;
+			String fileName = "simple.txt";
+			String path = Paths.get("").toAbsolutePath().toString()+"/test_graphs/" + fileName;
 			Scanner fin;
 			fin = new Scanner(new File(path));
             String name = fin.nextLine();
